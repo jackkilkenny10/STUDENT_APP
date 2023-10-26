207683e2697915be4cffe7598362f8f4be6a779f
jackkilkenny10 <G00401535@atu.ie>
2023-10-26 20:32:14 +0100

    STUDENT DETAILS FOR TWO USERS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,19 +12,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[]args){
+        String firstName;
         Scanner scanner = new Scanner(System.in);
         ie.atu.student user1 = new ie.atu.student();
 
 
         System.out.print("Enter First Name: ");
         user1.setStudentName(scanner.nextLine());
-        System.out.print("Enter Email Name: ");
+        System.out.print("Enter Email: ");
         user1.setStudentEmail(scanner.nextLine());
         System.out.print("Enter Course Name: ");
         user1.setCourseName(scanner.nextLine());
 
+        System.out.print("Enter First Name: ");
+        firstName = scanner.nextLine();
+        ie.atu.student user2 = new ie.atu.student(firstName);
+        System.out.print("Enter Email: ");
+        user2.setStudentBEmail(scanner.nextLine());
+        System.out.print("Enter Course Name: ");
+        user2.setCourseBName(scanner.nextLine());
 
-        user1.displayInfo();
 
+        user1.displayInfo1();
+        user2.displayInfo2();
     }
 }
